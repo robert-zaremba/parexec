2f41db9222c98e9887f30ce855efac5872c87852
Robert Zaremba <robert.zaremba@zoho.com>
2014-10-27 21:27:27 +0100

    Added tests and example for semaphore

--- semaphore.go
+++ semaphore.go
@@ -3,37 +3,42 @@ package parexec
 import (
 	"strconv"
 	"sync"
+	"time"
 )
 
-type Semaphore struct {
+type semaphore struct {
 	max      int
 	acquired int
 	cond     *sync.Cond
 }
 
-func NewSemaphore(max int) *Semaphore {
+// NewSemaphore returns new Semaphore, which implements a semaphore pattern.
+// It is used to limit the number of used resources to `max`. To ask for a resource
+// you firstly need to call `Asquire` and when you finish a job you need to release
+// a right for resource by calling `Release`.
+func NewSemaphore(max int) *semaphore {
 	if max <= 0 {
 		panic("number of available resources must be positive")
 	}
-	return &Semaphore{
+	return &semaphore{
 		max:  max,
 		cond: sync.NewCond(new(sync.Mutex)),
 	}
 }
 
 // Acquire locks 1 resource. Blocks if there not enought free resources
-func (s *Semaphore) Acquire() {
+func (s *semaphore) Acquire() {
 	s.AcquireN(1)
 }
 
 // AcquireN locks `n` resources. Blocks if there not enought free resources
-func (s *Semaphore) AcquireN(n int) {
+func (s *semaphore) AcquireN(n int) {
 	if n > s.max {
 		panic("can't acquire more resources then `max`=" + strconv.Itoa(s.max))
 	}
 	s.cond.L.Lock()
 	s.acquired += n
-	for s.acquired >= s.max {
+	for s.acquired > s.max {
 		s.cond.Wait()
 	}
 	s.cond.L.Unlock()
@@ -41,53 +46,63 @@ func (s *Semaphore) AcquireN(n int) {
 
 // AcquireCancel is like AcquireN with option to send a signal to cancel the
 // request. Returns true if successful and false if cancel occurs.
-// One useful use-case is a timeout:
-//     timeout := make(chan bool)
-//     go func() {
-//         time.Sleep(d)
-//         timeout <- true
-//     }()
-//     s.AcquireCancel(2, timeout)
-func (s *Semaphore) AcquireCancel(n int, cancel <-chan struct{}) bool {
-	s.cond.L.Lock()
-	defer s.cond.L.Unlock()
-
+// Check `AcquireTimeout` implementation for an example use.
+func (s *semaphore) AcquireCancel(n int, cancel <-chan struct{}) bool {
 	var ok = make(chan struct{})
-	var canceled = false
+	var notCanceled = true
 	go func() {
-		for s.acquired+n >= s.max && !canceled {
+		s.cond.L.Lock()
+		for s.acquired+n > s.max && notCanceled {
 			s.cond.Wait()
 		}
+		if notCanceled {
+			s.acquired += n
+		}
+		s.cond.L.Unlock()
 		ok <- struct{}{}
 	}()
 	select {
 	case <-ok:
-		s.acquired += n
-		return true
 	case <-cancel:
-		canceled = true
-		return false
+		notCanceled = false
+		s.cond.Signal()
 	}
+	return notCanceled
+}
+
+// AcquireTimeout is a a usefull pattern to acquire a resource with timetout
+// after `d` duration. If duration is super small (ns magnitude) then there is
+// a risk that a code will not have a chance to ask for resource befor timeout.
+// It's implemented using `AcquireCancel`
+func (s *semaphore) AcquireTimeout(n int, d time.Duration) bool {
+	timeout := make(chan struct{})
+	go func() {
+		time.Sleep(d)
+		timeout <- struct{}{}
+	}()
+	return s.AcquireCancel(n, timeout)
 }
 
 // Release unlocks 1 resources.
-func (s *Semaphore) Release() {
+func (s *semaphore) Release() {
 	s.ReleaseN(1)
 }
 
 // ReleaseN unlocks `n` resources.
-func (s *Semaphore) ReleaseN(n int) {
+func (s *semaphore) ReleaseN(n int) {
 	s.cond.L.Lock()
 	s.acquired -= n
 	if s.acquired <= 0 {
-		s.cond.Signal()
 		s.acquired = 0
+	}
+	if s.acquired <= s.max {
+		s.cond.Signal()
 	}
 	s.cond.L.Unlock()
 }
 
 // Wait waits till all resaurces will be available
-func (s *Semaphore) Wait() {
+func (s *semaphore) Wait() {
 	s.cond.L.Lock()
 	for s.acquired != 0 {
 		s.cond.Wait()
@@ -96,7 +111,7 @@ func (s *Semaphore) Wait() {
 }
 
 // Available returns number of available resources
-func (s *Semaphore) Available() int {
+func (s *semaphore) Available() int {
 	n := s.max - s.acquired
 	if n < 0 {
 		return 0
---
+++ semaphore_test.go
@@ -0,0 +1,40 @@
+package parexec
+
+import (
+	"fmt"
+	"testing"
+)
+
+func ExampleNewSemaphore() {
+	sem := NewSemaphore(1)
+	total := 4
+	job := func(i int) {
+		fmt.Println(i)
+		sem.Release()
+	}
+	for i := 0; i < total; i++ {
+		sem.Acquire()
+		go job(i)
+	}
+	sem.Wait()
+	// Output:
+	// 0
+	// 1
+	// 2
+	// 3
+}
+
+func TestAcquireTimeout(t *testing.T) {
+	sem := NewSemaphore(1)
+	sem.Acquire()
+	ok := sem.AcquireTimeout(1, 10000)
+	if ok {
+		t.Error("Should be timeouted")
+	}
+	sem.Release()
+	ok = sem.AcquireTimeout(1, 10000)
+	if !ok {
+		t.Error("Shouldn't be timeouted")
+	}
+	sem.Release()
+}
